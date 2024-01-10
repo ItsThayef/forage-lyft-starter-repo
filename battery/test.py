a0612aaf3f2bbd58ff9853bd952c07d01fa5e78d
ItsThayef <psn.thayef@gmail.com>
2024-01-10 22:46:51 +0000

    Task 3 - check current unit tests

--- battery/test.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-
-s = date.today()-date(2003,1,5)
-y = s.days/365
-print(y)
-#print(y < 20)
-
-#------
--- test/test_car.py
+++ test/test_car.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, date
 
 from engine.model.calliope import Calliope
 from engine.model.glissade import Glissade
